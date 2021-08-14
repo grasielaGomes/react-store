e9ee9b851dc875d6043ce3c3671afdfc3296748a
Grasiela Gomes <produtoradama@gmail.com>
2021-08-14 18:21:38 -0300

    cart icon

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { auth } from '../../firebase/firebase.utils';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
+import CartIcon from '../cart-icon/cart-icon.component';
 import './header.styles.scss';
 
 const Header = ({ currentUser }) => (
@@ -24,6 +25,7 @@ const Header = ({ currentUser }) => (
           </div>
           : <Link className='option' to='/signin'>SIGN IN</Link>
       }
+      <CartIcon />
     </div>
   </div>
 );
