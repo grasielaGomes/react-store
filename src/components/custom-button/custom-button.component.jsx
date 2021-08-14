dfa8b7322dfeffab8f9c388091a6e10804c88291
Grasiela Gomes <produtoradama@gmail.com>
2021-08-14 19:12:27 -0300

    added style and button to collection item

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './custom-button.styles.scss';
 
-const CustomButton = ({ children, isGoogleSignIn,  ...otherProps}) => (
-  <button className={`${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`} {...otherProps}>
+const CustomButton = ({ children, isGoogleSignIn, inverted, ...otherProps}) => {
+  const isGoogle = `${isGoogleSignIn ? 'google-sign-in' : ''}`;
+  const isInverted = `${inverted ? 'inverted' : ''}`;
+  return <button className={`${isGoogle} ${isInverted} custom-button`} {...otherProps}>
     {children}
   </button>
-);
+};
 
 export default CustomButton;
