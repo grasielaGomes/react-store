117e6c451fcfcdac22f012ec7ee1d46c85fe7a0a
Grasiela Gomes <produtoradama@gmail.com>
2021-08-15 16:45:01 -0300

    create checkout items

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
+import CheckouItem from '../../components/checkout-item/checkout-item.component';
 import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selectors';
 import './checkout.styles.scss';
 
@@ -9,11 +10,14 @@ const CheckoutPage = ({cartItems, total}) => (
     <div className="checkout-header">
       <div className="header-block">
         <span>Product</span>
+        <span>Description</span>
+        <span>Price</span>
+        <span>Remove</span>
       </div>
     </div>
     {
       cartItems.map(cartItem => 
-        cartItem.name)
+        <CheckouItem key={cartItem.id} cartItem={cartItem}/>)
     }
     <div className="total">
       <span>TOTAL: ${total}</span>
