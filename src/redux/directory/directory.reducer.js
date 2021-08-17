4c147dec58f35f04d16a48f98d9c0c514956a51f
Grasiela Gomes <produtoradama@gmail.com>
2021-08-17 20:18:15 -0300

    redux in collection and directory

---
+++ src/redux/directory/directory.reducer.js
@@ -0,0 +1,14 @@
+import SECTION_DATA from '../../data/sections.data';
+
+const INITIAL_STATE = {
+  sections: SECTION_DATA
+}
+
+const directoryReducer = (state = INITIAL_STATE, action) => {
+  switch(action.type) {
+    default:
+      return state;
+  }
+};
+
+export default directoryReducer;
